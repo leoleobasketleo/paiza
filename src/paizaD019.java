52d9d4c8a5886bc15d007c8fa736e6ee8493cd26
Leo Furutachi <reo@weseek.co.jp>
2018-09-25 13:58:19 +0900

    paizaD019

--- src/paizaD019.java
+++ src/paizaD019.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.*;
+
 
 public class paizaD019 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        String[] lineArray = line.split(" ",2);
+        int N = Integer.parseInt(lineArray[1]);
+        String[] Array = line.split("",line.length());
+        System.out.println(Array[N-1]);
+    }
 }
