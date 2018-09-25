348ddbca0d413a58827632f5761086cbd510a101
Leo Furutachi <reo@weseek.co.jp>
2018-09-26 08:13:17 +0900

    paizaD023

--- src/paizaD023.java
+++ src/paizaD023.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.*;
+
 
 public class paizaD023 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+
+        int num = 0;
+
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        String[] lineArray = line.split("",line.length());
+
+        for(int i = 0;i<lineArray.length;i++){
+            if(lineArray[i].equals("A"))
+                num++;
+        }
+        System.out.println(num);
+    }
 }
