9f4aad27aa488707d3f9145c02793f1a1973d3ed
Leo Furutachi <reo@weseek.co.jp>
2018-09-25 13:58:05 +0900

    paizaD017

--- src/paizaD017.java
+++ src/paizaD017.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.*;
+
 
 public class paizaD017 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int[] Array = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0;i<5;i++){
+            Array[i] = sc.nextInt();
+        }
+        int max = Array[0];
+        int min = Array[0];
+        for(int i = 0;i<5;i++){
+          max = Math.max(max,Array[i]);
+          min = Math.min(min,Array[i]);
+        }
+
+        System.out.println(max);
+        System.out.println(min);
+
+    }
 }
